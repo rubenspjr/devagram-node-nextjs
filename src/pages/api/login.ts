ef71bdc6a36761113ea012ddeaaf97e8f63a2d08
Rubens Pinheiro <torettodev@gmail.com>
2023-04-10 16:16:31 -0300

    Inicio da integraçao JWT

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,15 +1,36 @@
 import type{NextApiRequest, NextApiResponse}from 'next';
 import { conectarMongoDB } from '../../../middleware/conectarMongoDB';
 import type {respostaPadraoMsg} from '../../../types/respostaPadraoMsg'
+import md5 from 'md5';
+import { usuarioModels } from '../../../models/usuarioModels';
+import jwt from 'jsonwebtoken';
+import type {loginResposta} from '../../../types/loginResposta'
 
-const endpointLogin =  (req:NextApiRequest, res:NextApiResponse<respostaPadraoMsg>) => {
+
+const endpointLogin =  
+    async (req:NextApiRequest, res:NextApiResponse<respostaPadraoMsg | loginResposta>) => {
+
+        const {MINHA_CHAVE_JWT} = process.env;
+
+        if(!MINHA_CHAVE_JWT){
+            return res.status(500).json({erro : 'ENV Jwt nao informada'});
+        }
+
     if(req.method === 'POST'){
-        const {login, senha} = req.body
-        if( login === 'admin@admin.com' && 
-                senha === 'Admin@123'){
-                   return res.status(200).json({msg:'Usuario autenticado com sucesso'})
+        const {login, senha} = req.body;
 
-                    }
+        const usuariosEncontrados = await usuarioModels.find ({email:login,senha : md5(senha)})
+        if( usuariosEncontrados && usuariosEncontrados.length > 0){
+                    const usuarioEncontado =  usuariosEncontrados[0];
+
+         const token = jwt.sign({_id: usuarioEncontado._id}, MINHA_CHAVE_JWT);
+         
+                   return res.status(200).json({
+                    nome : usuarioEncontado.nome,
+                    email : usuarioEncontado.email,
+                    token})
+
+        }
 
         return res.status(400).json({erro: 'Usuario ou senha invalidos'})
 
