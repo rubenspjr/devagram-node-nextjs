cc12a7ef84759bba1b8bc8377ca07c2615b5e752
Rubens Pinheiro <torettodev@gmail.com>
2023-05-12 13:56:12 -0300

    Update usuarioModels.ts

--- models/usuarioModels.ts
+++ models/usuarioModels.ts
@@ -2,7 +2,7 @@ import mongoose, {Schema} from 'mongoose'
 
 const usuarioSchema = new Schema({
     nome: {type : String, required : true},
-    email: {type : String, require : true},
+    email: {type : String, required : true},
     senha: {type : String, required :true},
     avatar : {type : String, required : false},
     seguidores : {type : Number, default : 0 },
