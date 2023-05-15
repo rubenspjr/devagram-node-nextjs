0d979281b2cc00bc41fad3f89ce51cd45354447b
Rubens Pinheiro <torettodev@gmail.com>
2023-05-15 08:36:32 -0300

    Update seguidorMoldels.ts

--- models/seguidorMoldels.ts
+++ models/seguidorMoldels.ts
@@ -4,7 +4,9 @@ const seguidorSchema = new Schema({
     //quem ssegue
     usuarioId : {type : String, required : true},
     //quem esta sendo seguido
-    usuarioSeguidoId : {type : String, required : true}
+    usuarioSeguidoId : {type : String, required : true},
+
+    segueEsseUsuario :{type : String, required : true}
 });
 export const seguidorModels = (mongoose.models.seguidores || 
     mongoose.model('seguidores', seguidorSchema));
